0c11f9a8f4c72f8d4d19dd6db9cca81431f7652a
jakov-civcija <jakov.civcijacikeve@gmail.com>
2018-09-15 21:00:25 +0200

    Napravili migraciju

--- src/Controller/GopnikProductController.php
+++ src/Controller/GopnikProductController.php
@@ -11,6 +11,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
+//REST API
 /**
  * @Route("/gopnik/product")
  */
